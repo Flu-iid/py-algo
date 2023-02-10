91b80c6738c50a51ca459fe105f221c40b42d1f6
Flu <57827666+Flu-iid@users.noreply.github.com>
2023-02-10 15:08:57 +0330

    added theory-of-computation

--- __test/01.py
+++ __test/01.py
@@ -9,4 +9,15 @@
 #     print(i)
 #     print(eval("string."+i))
 #     print("-------")
-print([4, 3, 2, 1].sort())
+# print([4, 3, 2, 1].sort())
+
+# a = {(1, 1), (1, 2)}
+# b = {(1, 1)}
+# c = a.difference(b)
+
+# print(c)
+# n, m = 2, 3
+
+# all_key_set = {(a, b) for a in range(1, n+1) for b in range(1, m+1)}
+
+# print(all_key_set)
