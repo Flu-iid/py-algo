fc9467ec48c443fa3cab0b72c868802b1ae2611e
Flu <57827666+Flu-iid@users.noreply.github.com>
2023-04-05 11:52:27 +0330

    shamir problem first attemp

--- algo-questions/quera/Shamir’s Birthday Party.py
+++ algo-questions/quera/Shamir’s Birthday Party.py
@@ -1,6 +1,19 @@
+def check(graph):
+    count = 0
+    for n in graph:
+        if not graph[n]:
+            return 1
+
+
 for _ in range(int(input())):
     n, m = map(int, input().split())
     crush = set()
     for _ in range(m):
         u, v = map(int, input().split())
-        crush.add(tuple(u, v))
+        crush.add((u, v))
+    graph = {}
+    for edge in crush:
+        p1, p2 = edge
+        graph.setdefault(p1, set())
+        graph.setdefault(p2, set())
+        graph[p1].add(p2)
