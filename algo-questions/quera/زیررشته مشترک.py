31224882826b175a1bd0b870d71c9e90abb12db9
flu <57827666+Flu-iid@users.noreply.github.com>
2023-07-11 11:41:09 +0330

    new problem today _unsolved_

---
+++ algo-questions/quera/زیررشته مشترک.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+
+def check_pattern(s1, s2):
+    for i, c1 in enumerate(s1):
+        for j, c2 in enumerate(s2):
+            if i == j:
+                pass
+
+# cmplicated
+
+
+l = [input() for _ in range(int(input()))]
