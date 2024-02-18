10084f9ee99fbf54fffb9144b711a238cc175add
flu <57827666+Flu-iid@users.noreply.github.com>
2024-02-18 15:36:25 +0330

    added new answer for codeforces 1800A is it a cat

--- algo-questions/codeforces/A_Is_It_a_Cat.py
+++ algo-questions/codeforces/A_Is_It_a_Cat.py
@@ -1,21 +1,37 @@
-t = int(input())
+# t = int(input())
 
 
-def check(s, n):
-    check_list = list("meow")
-    result = []
-    if s[0] != "m" or s[-1] != "w":
-        return False
-    else:
-        for i in range(n):
-            if i == 0:
-                result.append(s[i])
-            elif s[i] != result[-1]:
-                result.append(s[i])
-    return result == check_list
+# def check(s, n):
+#     check_list = list("meow")
+#     result = []
+#     if s[0] != "m" or s[-1] != "w":
+#         return False
+#     else:
+#         for i in range(n):
+#             if i == 0:
+#                 result.append(s[i])
+#             elif s[i] != result[-1]:
+#                 result.append(s[i])
+#     return result == check_list
 
 
-for i in range(t):
+# for i in range(t):
+#     n = int(input())
+#     s = input().lower()
+#     print("YES") if check(s, n) else print("NO")
+
+
+# 2
+def accept(s: str, n: int):
+    guide = "meow"
+    check = ""
+    for i in range(n):
+        check += s[i].lower() if i == 0 or s[i -
+                                             1].lower() != s[i].lower() else ""
+    return check == guide
+
+
+for _ in range(int(input())):
     n = int(input())
-    s = input().lower()
-    print("YES") if check(s, n) else print("NO")
+    s = input()
+    print("YES" if accept(s, n) else "NO")
